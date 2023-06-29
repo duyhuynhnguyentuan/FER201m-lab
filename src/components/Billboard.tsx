e1eb7988c391af30e582729153dfdb84420c0cb6
DUY HUYNH <124075895+duyhuynhnguyentuan@users.noreply.github.com>
2023-06-29 17:04:20 +0700

    autoplay for mobile screen

--- src/components/Billboard.tsx
+++ src/components/Billboard.tsx
@@ -25,7 +25,8 @@ const Billboard:React.FC<BillboardProps> = ({film})=>{
     }
 return(
     <div className="relative h-[56.25vw]">
-    <video poster={film.thumbnailUrl} className="w-full h-[56.25vw] object-cover brightness-[60%] transition duration-500" autoPlay muted loop src={film.videoUrl}></video>
+  <video poster={film.thumbnailUrl} className="w-full h-[56.25vw] object-cover brightness-[60%] transition duration-500" autoPlay muted loop playsInline src={film.videoUrl}></video>
+
     <div className="absolute top-[30%] md:top-[40%] ml-4 md:ml-16">
       <p className="text-white text-1xl md:text-5xl h-full w-[50%] lg:text-6xl font-bold drop-shadow-xl">
         {film.title}
